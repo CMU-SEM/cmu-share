21d4d0dc6f074b4546dd9b4a05201803fae2809f
เบน <pattarasaimarkpeng@pattarasais-macbook-pro.local>
2018-10-01 15:25:41 -0700

    Initial Commit

---
+++ cmu-share/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cmu-share
+//
+//  Created by Pattarasai Markpeng on 10/1/18.
+//  Copyright © 2018 Pattarasai Markpeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
