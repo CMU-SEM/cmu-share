9e3d72b2c94940dccccd1fe4dc69a484ccba83ca
Bethany Huang <bethany@bethanys-macbook-pro.local>
2018-10-03 01:17:06 -0700

    Sign in / up basic finished

--- cmu-share/ViewController.swift
+++ cmu-share/ViewController.swift
@@ -7,9 +7,49 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBAction func logInAction(_ sender: UIButton) {
+        if emailText.text != "" && passwordText.text != ""{
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { (user, error) in
+                if user != nil {
+                    // sign in successful
+                }
+                else {
+                    if let myError = error?.localizedDescription {
+                        print(myError)
+                    }else{
+                        print("ERROR")
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func signUpAction(_ sender: UIButton) {
+        if emailText.text != "" && passwordText.text != ""{
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (authResult, error) in
+                if authResult != nil {
+                    // sign up successful
+                }
+                else {
+                    if let myError = error?.localizedDescription {
+                        print(myError)
+                    }else{
+                        print("ERROR")
+                    }
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
