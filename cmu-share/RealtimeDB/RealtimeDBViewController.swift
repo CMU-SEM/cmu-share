68633277f71675d75407a3bef2a9ca13fbce3cc4
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-10-06 14:47:59 -0700

    - Add RealtimeDBController

---
+++ cmu-share/RealtimeDB/RealtimeDBViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RealtimeDBViewController.swift
+//  cmu-share
+//
+//  Created by Pattarasai Markpeng on 10/6/18.
+//  Copyright © 2018 Pattarasai Markpeng. All rights reserved.
+//
+
+import UIKit
+
+class RealtimeDBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
