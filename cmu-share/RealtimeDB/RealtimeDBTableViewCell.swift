b03b6fe99c83196fa2fbaf1cdac519f372ed3572
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-10-06 16:20:59 -0700

    - Implement table

---
+++ cmu-share/RealtimeDB/RealtimeDBTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  RealtimeDBTableViewCell.swift
+//  cmu-share
+//
+//  Created by Pattarasai Markpeng on 10/6/18.
+//  Copyright © 2018 Pattarasai Markpeng. All rights reserved.
+//
+
+import UIKit
+
+class RealtimeDBTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- cmu-share/RealtimeDB/RealtimeDBViewController.swift
+++ cmu-share/RealtimeDB/RealtimeDBViewController.swift
@@ -9,19 +9,29 @@
 import UIKit
 import Firebase
 
-class RealtimeDBViewController: UIViewController {
-
+class RealtimeDBViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
     @IBOutlet weak var nameLabel: UITextField!
     @IBOutlet weak var detailLabel: UITextField!
-    
-    
+    @IBOutlet weak var tableView: UITableView!
     var ref: DatabaseReference!
+    
+    var orderList = [Order]();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         ref = Database.database().reference()
         // Do any additional setup after loading the view.
+        loadOrders()
     }
     
+    func loadOrders() {
+        let order1 = Order(name: "Mc", detail: "Eiei")
+        orderList.append(order1)
+        let order2 = Order(name: "Mc", detail: "Eiei")
+        orderList.append(order2)
+        tableView.reloadData()
+    }
 
     @IBAction func addRestaurant(_ sender: Any) {
         let orderRef = Database.database().reference().child("orders").childByAutoId();
@@ -37,14 +47,18 @@ class RealtimeDBViewController: UIViewController {
         }        
     }
     
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return orderList.count;
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellOrder", for: indexPath) as! RealtimeDBTableViewCell
+        
+        let order = orderList[indexPath.row] as Order
+        cell.nameLabel.text = order.name;
+        cell.detailLabel.text = order.detail;
+        return cell;
+    }
 
 }
