bfdcdc467caf8f0992ba7b137ec5f6f687657041
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-10-16 12:52:12 -0700

    add table

--- cmu-share/FeedController.swift
+++ cmu-share/FeedController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class feedController: UIViewController {
+class feedController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var createButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +22,14 @@ class feedController: UIViewController {
         self.performSegue(withIdentifier: "FeedToCreateSegue", sender: self)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 0;
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellOrder", for: indexPath) as! FeedTableViewCell;
+        return cell;
+    }
+    
 
 }
---
+++ cmu-share/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  cmu-share
+//
+//  Created by Pattarasai Markpeng on 10/16/18.
+//  Copyright © 2018 Pattarasai Markpeng. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
