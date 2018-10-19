386eb404f81e204f96f1ae2a86fab485bca64962
bethany <hlxhlx1996@126.com>
2018-10-18 20:39:53 -0700

    join button transaction completed

--- cmu-share/Cells/FeedTableViewCell.swift
+++ cmu-share/Cells/FeedTableViewCell.swift
@@ -1,3 +1,5 @@
+
+
 //
 //  FeedTableViewCell.swift
 //  cmu-share
@@ -8,7 +10,11 @@
 
 import UIKit
 
+protocol FeedCellDelegate: class {
+    func didTapJoin(_ sender: FeedTableViewCell)
+}
 class FeedTableViewCell: UITableViewCell {
+    weak var delegate: FeedCellDelegate?
     
     @IBOutlet weak var viewWrapper: UIView!
     @IBOutlet weak var creatorName: UILabel!
@@ -16,17 +22,19 @@ class FeedTableViewCell: UITableViewCell {
     @IBOutlet weak var orderTime: UILabel!
     @IBOutlet weak var numOfPeople: UILabel!
     @IBOutlet weak var deliveryFee: UILabel!
-
+    
     @IBOutlet weak var joinButton: UIButton!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-    
+    @IBAction func joinAction(_ sender: UIButton) {
+        delegate?.didTapJoin(self)
+    }
 }
