14776b4228908da5b85e537f8cd30a7a3c8ff8bf
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-10-21 10:28:03 -0700

    style view wrapper

--- cmu-share/joinOrderController.swift
+++ cmu-share/joinOrderController.swift
@@ -11,6 +11,7 @@ import Firebase
 
 class joinOrderController: UIViewController {
 
+    @IBOutlet weak var viewWrapper: UIView!
     @IBOutlet weak var foodItemName1: UITextField!
     @IBOutlet weak var quantity1: UITextField!
     @IBOutlet weak var size1: UITextField!
@@ -26,5 +27,17 @@ class joinOrderController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        styleViewWrapper(viewWrapper: self.viewWrapper);
+    }
+    
+    func styleViewWrapper(viewWrapper: UIView) {
+        viewWrapper.layer.borderColor = UIColor.lightGray.cgColor;
+        viewWrapper.layer.borderWidth = 0.3;
+        viewWrapper.layer.cornerRadius = 5;
+        
+        viewWrapper.layer.shadowColor = UIColor.lightGray.cgColor
+        viewWrapper.layer.shadowOpacity = 0.8
+        viewWrapper.layer.shadowOffset = CGSize(width: -1, height: 1)
+        viewWrapper.layer.shadowRadius = 4
     }
 }
