6905e21fd43b143e8b76a7133916dc6c83f87cda
Sathish <sathish@Sathishs-Mac.local>
2018-11-03 20:59:01 -0700

    Connected FIrebase to Profile screen and Fixed Button Layouts

--- cmu-share/profileController.swift
+++ cmu-share/profileController.swift
@@ -11,10 +11,37 @@ import Firebase
 
 class profileController: UIViewController {
 
+    @IBOutlet weak var firstName: UILabel!
+    @IBOutlet weak var lastName: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var phoneNumber: UILabel!
+    
+    
     @IBOutlet weak var signOutButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var ref: DatabaseReference!
+        ref = Database.database().reference();
+        
+        let user = Auth.auth().currentUser
+        let creator_id = user!.uid
+        
+        ref.child("users").child(creator_id).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user profile info
+            let profile = snapshot.value as! [String: AnyObject]
+            let firstName = profile["firstName"]
+            let lastName = profile["lastName"]
+            let email = profile["email"]
+            let phoneNumber = profile["phoneNumber"]
+            self.firstName.text = firstName as? String
+            self.lastName.text = lastName as? String
+            self.email.text = email as? String
+            self.phoneNumber.text = phoneNumber as? String
+            // ...
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
     @IBAction func signOutAction(_ sender: UIButton) {
         // sign off
